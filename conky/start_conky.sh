ed09b0935cdb387348083a03e8fedefc24f27481
Budiman JoJo <budimanjojo@gmail.com>
2016-08-02 15:35:09 +0700

    enable clock

--- conky/start_conky.sh
+++ conky/start_conky.sh
@@ -3,4 +3,4 @@ conky -c ~/.conkyrc/.conkyrc_cpupanel &
 conky -c ~/.conkyrc/.conkyrc_nvidia &
 conky -c ~/.conkyrc/.conkyrc_processpanel &
 conky -c ~/.conkyrc/.conkyrc_tempfan &
-#conky -c ~/.conkyrc/.conkyrc_clock &
+conky -c ~/.conkyrc/.conkyrc_clock &
