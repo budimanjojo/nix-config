f071b4f449e017b1a9101e83c681f193dfba0831
budimanjojo <budimanjojo@gmail.com>
2021-11-16 09:09:32 +0000

    small tweaks

--- install.sh
+++ install.sh
@@ -17,7 +17,4 @@ else
   chezmoi=chezmoi
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+exec "$chezmoi" init --apply budimanjojo
