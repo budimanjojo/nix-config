b67c9991111882ba9a30477a0fb3440ad9ac00cc
budimanjojo <budimanjojo@gmail.com>
2021-12-03 16:59:39 +0700

    fix error in colorscheme before install

--- dot_config/nvim/lua/appearance.lua
+++ dot_config/nvim/lua/appearance.lua
@@ -1,4 +1,10 @@
-vim.go.background = 'dark' -- Dark background
-vim.go.termguicolors = true -- Enable 256 colors
-vim.go.signcolumn = 'yes' -- Always show signcolumn
-vim.g.colors_name = 'base16-google-dark' -- Colorscheme theme
+vim.go.background = 'dark'   -- Dark background
+vim.go.termguicolors = true  -- Enable 256 colors
+vim.go.signcolumn = 'yes'    -- Always show signcolumn
+
+-- Colorscheme
+local theme_name = 'nvim-base16'
+local packer_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/'
+if vim.fn.isdirectory(packer_path .. theme_name) == 1 then
+  vim.cmd('colorscheme base16-google-dark')
+end
