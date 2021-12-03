1621f10f0af0b5c67508e52f759897661e9373aa
budimanjojo <budimanjojo@gmail.com>
2021-12-03 16:13:19 +0700

    use colors_name

--- dot_config/nvim/lua/appearance.lua
+++ dot_config/nvim/lua/appearance.lua
@@ -1,4 +1,4 @@
 vim.go.background = 'dark' -- Dark background
 vim.go.termguicolors = true -- Enable 256 colors
 vim.go.signcolumn = 'yes' -- Always show signcolumn
-vim.cmd('colorscheme base16-google-dark') -- Colorscheme theme
+vim.g.colors_name = 'base16-google-dark' -- Colorscheme theme
