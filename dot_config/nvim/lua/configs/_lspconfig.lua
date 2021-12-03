f4044d856fa9209c2e7469e7dfe4c31a72251c41
budimanjojo <budimanjojo@gmail.com>
2021-12-03 16:50:10 +0700

    auto install lsp

--- dot_config/nvim/lua/configs/_lspconfig.lua
+++ dot_config/nvim/lua/configs/_lspconfig.lua
@@ -1,3 +1,23 @@
+local lsp_installer_servers = require('nvim-lsp-installer.servers')
+-- List of LSP I want to be installed
+local requested_servers = {
+  'bashls',
+  'dockerls',
+  'jsonls',
+  'sumneko_lua',
+  'yamlls'
+}
+
+-- Auto install requested LSP
+for _, i in ipairs(requested_servers) do
+  local server_available, requested_server = lsp_installer_servers.get_server(i)
+  if server_available then
+    if not requested_server:is_installed() then
+      requested_server:install()
+    end
+  end
+end
+
 require('configs.lspservers._bashls')
 require('configs.lspservers._dockerls')
 require('configs.lspservers._jsonls')
