71ca0483c54bf8fceee61b7f65daf891611551d9
budimanjojo <budimanjojo@gmail.com>
2021-12-05 17:23:16 +0700

    add rules

--- dot_config/nvim/lua/configs/_autopairs.lua
+++ dot_config/nvim/lua/configs/_autopairs.lua
@@ -6,3 +6,31 @@ npairs.setup{}
 npairs.add_rule(
   Rule('<','>')
 )
+
+-- (|) press space => ( | )
+-- ( | ) press ) => ( )|
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '{}', '[]' }, pair)
+    end),
+  Rule('( ', ' )')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+  Rule('{ ', ' }')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+  Rule('[ ', ' ]')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+}
