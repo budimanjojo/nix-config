369a0e0af3522a419d798b8381b1ad2b0ca12eb2
budimanjojo <budimanjojo@gmail.com>
2021-12-09 16:39:00 +0700

    this messed up enter key

--- dot_config/nvim/lua/configs/_nvim-cmp.lua
+++ dot_config/nvim/lua/configs/_nvim-cmp.lua
@@ -2,17 +2,10 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local vssnip = require('luasnip.loaders.from_vscode')
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
 
 vssnip.lazy_load()
 
 cmp.setup {
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
   formatting = {
     format = lspkind.cmp_format({
       with_text = true,
