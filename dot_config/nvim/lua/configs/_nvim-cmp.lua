374ee78ed62ba9ee2813f96de7761d57f2f91e6e
budimanjojo <budimanjojo@gmail.com>
2021-12-09 14:05:49 +0700

    this breaks tab

--- dot_config/nvim/lua/configs/_nvim-cmp.lua
+++ dot_config/nvim/lua/configs/_nvim-cmp.lua
@@ -50,8 +50,6 @@ cmp.setup {
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
