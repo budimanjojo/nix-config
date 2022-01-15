7e5cefa8adcd2ccc2a7addd97d237b65d032d7bf
budimanjojo <budimanjojo@gmail.com>
2022-01-16 00:50:59 +0700

    nvim-cmp overhaul

--- dot_config/nvim/lua/configs/_nvim-cmp.lua
+++ dot_config/nvim/lua/configs/_nvim-cmp.lua
@@ -3,60 +3,66 @@ local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local vssnip = require('luasnip.loaders.from_vscode')
 
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menu,menuone,noselect'
 vssnip.lazy_load()
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup {
-  formatting = {
-    format = lspkind.cmp_format({
-      with_text = true,
-      menu = ({
-        nvim_lsp = '[LSP]',
-        luasnip = '[LuaSnip]',
-        nvim_lua = '[Lua]',
-        buffer = '[Buffer]',
-        latex_symbols = '[Latex]',
-        path = '[Path]',
-        spell = '[Spell]'
-      })
-    }),
-  },
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
-    end
+    end,
+  },
+  mapping = {
+    ['<C-j>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ['<C-k>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ['<C-Space>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+    },
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+    },
   },
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
     { name = 'buffer' },
-    { name = 'spell' }
+    { name = 'spell' },
   },
-  mapping = {
-    ['<C-k>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ['<C-j>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
-    ['<C-Space>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-    },
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = ({
+        nvim_lsp = '[LSP]',
+        luasnip = '[LuaSnip]',
+        nvim_lua = '[Lua]',
+        buffer = '[Buffer]',
+        path = '[Path]',
+        spell = '[Spell]'
+      })
     },
   },
 }
