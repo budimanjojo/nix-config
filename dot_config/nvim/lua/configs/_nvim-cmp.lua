7c0022201299f273bf270b65a3f9a7cb2367481d
budimanjojo <budimanjojo@gmail.com>
2022-03-16 14:21:25 +0700

    feat(nvim): only jump in current snippet
    Refs: https://github.com/L3MON4D3/LuaSnip/wiki/Nice-Configs#only-jump-in-the-current-snippet

--- dot_config/nvim/lua/configs/_nvim-cmp.lua
+++ dot_config/nvim/lua/configs/_nvim-cmp.lua
@@ -21,7 +21,7 @@ cmp.setup {
     ['<C-j>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
