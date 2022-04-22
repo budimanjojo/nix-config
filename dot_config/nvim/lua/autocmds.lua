e1f1093f2a0f281de488c8063fb4b368f1a1d755
budimanjojo <budimanjojo@gmail.com>
2022-04-22 16:40:38 +0700

    feat(nvim): migrate autocommands to lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -24,38 +24,66 @@ api.nvim_create_autocmd('BufNewFile', {
 local disableautocomment = api.nvim_create_augroup('disableautocomment', { clear = true })
 api.nvim_create_autocmd( { 'BufEnter', 'CmdLineLeave' }, {
   pattern = '*',
-  command = 'set fo-=c fo-=r fo-=o',
+  callback = function()
+    vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
+  end,
   group = disableautocomment
 })
 
 -- Filetype detection
-cmd [[
-  augroup ansiblefiletype
-    autocmd!
-    autocmd BufRead,BufNewFile */playbooks/*.yml set filetype=yaml.ansible
-    autocmd BufRead,BufNewFile */playbooks/*.yaml set filetype=yaml.ansible
-    autocmd BufRead,BufNewFile */inventory/hosts.yml set filetype=yaml.ansible
-    autocmd BufRead,BufNewFile */inventory/hosts.yaml set filetype=yaml.ansible
-    autocmd BufRead,BufNewFile */.chezmoiscripts/*.sh.tmpl set filetype=sh.chezmoitmpl
-    autocmd BufRead,BufNewFile */.chezmoiscripts/*.fish.tmpl set filetype=fish.chezmoitmpl
-  augroup END
-]]
+local detectFt = function()
+  local fpath = vim.fn.expand '%:p'
+  if string.match(fpath, '.*/playbooks/.*%.yaml') or string.match(fpath, '.*/playbooks/.*%.yml') then
+    vim.o.filetype = 'yaml.ansible'
+  elseif string.match(fpath, '.*/inventory/hosts%.yaml') or string.match(fpath, '.*/inventory/hosts%.yml') then
+    vim.o.filetype = 'yaml.ansible'
+  elseif string.match(fpath, '.*/%.chezmoiscripts/.*%.sh.tmpl') then
+    vim.o.filetype = 'sh.chezmoitmpl'
+  elseif string.match(fpath, '.*/%.chezmoiscripts/.*%.fish.tmpl') then
+    vim.o.filetype = 'fish.chezmoitmpl'
+  end
+end
+
+local ftdetection = api.nvim_create_augroup('ftdetection', { clear = true })
+api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*',
+  callback = function()
+    vim.schedule(detectFt)
+  end,
+  group = ftdetection
+})
 
 -- Filetype configuration
-cmd [[
-  augroup filetypesettings
-    autocmd!
-    autocmd FileType go setlocal shiftwidth=4 tabstop=4 softtabstop=4 noexpandtab
-    autocmd FileType gitcommit setlocal colorcolumn=51,73
-  augroup END
-]]
+local ftconfiguration = api.nvim_create_augroup('ftconfiguration', { clear = true })
+api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = '*',
+  callback = function(arg)
+    local ft = arg.match
+    if ft == 'go' then
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.tabstop = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.expandtab = false
+    elseif ft == 'gitcommit' then
+      vim.opt_local.colorcolumn = {57, 73}
+    end
+  end,
+  group = ftconfiguration
+})
 
 -- ChoiceNode popup in snippet
-cmd([[
-  augroup choicepopup
-    autocmd!
-    autocmd User LuasnipChoiceNodeEnter lua require('utils').choice_popup(require('luasnip').session.event_node)
-    autocmd User LuasnipChoiceNodeLeave lua require('utils').choice_popup_close()
-    autocmd User LuasnipChangeChoice lua require('utils').update_choice_popup(require("luasnip").session.event_node)
-  augroup END
-]])
+local choicepopup = api.nvim_create_augroup('choicepopup', { clear = true })
+api.nvim_create_autocmd('User', {
+  pattern = { 'LuasnipChoiceNodeEnter', 'LuasnipChoiceNodeLeave', 'LuasnipChangeChoice' },
+  callback = function(arg)
+    local match = arg.match
+    if match == 'LuasnipChoiceNodeEnter' then
+      require('utils').choice_popup(require('luasnip').session.event_node)
+    elseif match == 'LuasnipChoiceNodeLeave' then
+      require('utils').choice_popup_close()
+    elseif match == 'LuasnipChangeChoice' then
+      require('utils').update_choice_popup(require('luasnip').session.event_node)
+    end
+  end,
+  group = choicepopup
+})
