eac7ce698a752d5b26a3bee3e3e0eb19b73f85ba
budimanjojo <budimanjojo@gmail.com>
2022-09-06 10:58:31 +0700

    feat(hyprland): [WIP] rename scripts

---
+++ dot_config/hypr/scripts/executable_autostart-applications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pamac-tray &
+blueman-applet &
+# nwg-dock -d -i 32 -o $monright &
+nm-applet --indicator &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# exec xfce4-power-manager
+# exec $HOME/.conkyrc/.conkyrci3/start_conky_i3.sh
+/usr/bin/nextcloud --background &
+exec "sleep 5; /usr/bin/syncthingtray" &
