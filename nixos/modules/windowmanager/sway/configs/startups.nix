a2e61c2809838beddc60a7e7adb045a03367faa7
budimanjojo <budimanjojo@gmail.com>
2022-11-19 16:25:17 +0700

    feat(sway): add inactive-window-transparency in nixos

--- nixos/modules/windowmanager/sway/configs/startups.nix
+++ nixos/modules/windowmanager/sway/configs/startups.nix
@@ -8,6 +8,7 @@
     ''; always = true;
   }
   { command = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"; always = false; }
+  { command = "${pkgs.sway-contrib.inactive-windows-transparency}/bin/inactive-windows-transparency.py -o 0.9"; always = true; }
   # {
   #   command = "systemctl --user import-environment";
   #   always = false;
