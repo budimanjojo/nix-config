d478596ce2685492fdc1ae90a4f330b09db2fa4b
budimanjojo <budimanjojo@gmail.com>
2023-02-09 10:21:43 +0700

    feat(nvim): update nvim-cmp keymap

--- nixos/modules/editor/neovim/lua/plugin-configs/_nvim-cmp.lua
+++ nixos/modules/editor/neovim/lua/plugin-configs/_nvim-cmp.lua
@@ -52,12 +52,24 @@ cmp.setup {
         fallback()
       end
     end, { "i", "s" }),
-    ['<C-Space>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-    },
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-    },
+    ['<C-Space>'] = cmp.mapping(function()
+      if luasnip.choice_active() then
+        luasnip.jump(1)
+      else
+        cmp.confirm {
+          behavior = cmp.ConfirmBehavior.Replace
+        }
+      end
+    end),
+    ['<CR>'] = cmp.mapping(function()
+      if luasnip.choice_active() then
+        luasnip.jump(1)
+      else
+        cmp.confirm {
+          behavior = cmp.ConfirmBehavior.Replace
+        }
+      end
+    end),
   },
   sources = {
     { name = 'nvim_lsp' },
