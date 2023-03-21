ee827ea0f92e82a3276e9fa3eb4eddcf8c3c26a6
budimanjojo <budimanjojo@gmail.com>
2023-03-22 00:44:28 +0700

    feat(swayidle): forgot to also change commands for hyprland

--- nixos/modules/windowmanager/add-on/swayidle/default.nix
+++ nixos/modules/windowmanager/add-on/swayidle/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, inputs, system, ... }:
 with lib;
 let
   cfg = config.modules.windowmanager.add-on.swayidle;
@@ -13,8 +13,14 @@ in {
       timeouts = [
         {
           timeout = 1800;
-          command = "${pkgs.sway}/bin/swaymsg \"output * dpms off\"";
-          resumeCommand = "${pkgs.sway}/bin/swaymsg \"output * dpms on\"";
+          command = (mkMerge [
+            (mkIf config.modules.windowmanager.sway.enable "${pkgs.sway}/bin/swaymsg \"output * dpms off\"")
+            (mkIf config.modules.windowmanager.hyprland.enable "${inputs.hyprland.packages.${system}.hyprland}/bin/hyprctl dispatch dpms off")
+          ]);
+          resumeCommand = (mkMerge [
+            (mkIf config.modules.windowmanager.sway.enable "${pkgs.sway}/bin/swaymsg \"output * dpms on\"")
+            (mkIf config.modules.windowmanager.hyprland.enable "${inputs.hyprland.packages.${system}.hyprland}/bin/hyprctl dispatch dpms on")
+          ]);
         }
       ];
       systemdTarget = mkIf config.modules.windowmanager.hyprland.enable "hyprland-session.target";
