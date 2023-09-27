70ccfec26d4511fbc8201d6bc9c3bc4e80937892
budimanjojo <budimanjojo@gmail.com>
2023-09-27 09:31:49 +0700

    feat(nvim): auto imports and format for golang

--- nixos/modules/editor/neovim/lua/autocmds.lua
+++ nixos/modules/editor/neovim/lua/autocmds.lua
@@ -61,6 +61,27 @@ api.nvim_create_autocmd({ 'FileType' }, {
   group = ftconfiguration
 })
 
+-- Auto imports and format on save for golang
+local autoimportformatgo = api.nvim_create_augroup('autoimportformatgo', { clear = true })
+api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({ async = false })
+  end,
+  group = autoimportformatgo
+})
+
 -- ChoiceNode popup in snippet
 local choicepopup = api.nvim_create_augroup('choicepopup', { clear = true })
 api.nvim_create_autocmd('User', {
