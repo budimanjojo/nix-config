5c6d7ea1d142bc0cde032b9f81e4c656662e247e
budimanjojo <budimanjojo@gmail.com>
2023-10-06 11:44:28 +0700

    feat(nixos): buildVimPluginFrom2Nix renamed to buildVimPlugin
    ref: https://github.com/NixOS/nixpkgs/pull/257487

--- nixos/packages/nvim-plugins/default.nix
+++ nixos/packages/nvim-plugins/default.nix
@@ -5,34 +5,34 @@ let
 in
 
 {
-  chezmoi-vim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  chezmoi-vim = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.chezmoi-vim) pname src;
     version = sourceData.chezmoi-vim.date;
   };
-  mason-lspconfig-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  mason-lspconfig-nvim = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.mason-lspconfig-nvim) pname src;
     version = sourceData.mason-lspconfig-nvim.date;
   };
-  mason-tool-installer-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  mason-tool-installer-nvim = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.mason-tool-installer-nvim) pname src;
     version = sourceData.mason-tool-installer-nvim.date;
   };
-  oil-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  oil-nvim = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.oil-nvim) pname src;
     version = sourceData.oil-nvim.date;
   };
-  luasnip = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  luasnip = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.luasnip) pname src version;
   };
-  sad-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  sad-nvim = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.sad-nvim) pname src;
     version = sourceData.sad-nvim.date;
   };
-  k8s-snippets = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  k8s-snippets = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.k8s-snippets) pname src;
     version = sourceData.k8s-snippets.date;
   };
-  guihua-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
+  guihua-lua = pkgs.vimUtils.buildVimPlugin {
     inherit (sourceData.guihua-lua) pname src;
     version = sourceData.guihua-lua.date;
     buildPhase = ''
