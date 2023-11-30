70bcf6c9e991484dc0cbb56e09bdf058cff8b6b3
budimanjojo <budimanjojo@gmail.com>
2023-11-30 14:43:28 +0700

    feat(nixos): unpin talosctl

--- nixos/packages/overlay.nix
+++ nixos/packages/overlay.nix
@@ -1,18 +1,18 @@
 { inputs, system, ... }:
 final: prev:
 {
-  talosctl = prev.talosctl.override {
-    buildGoModule = args: prev.buildGoModule (args // {
-      version = "1.5.1";
-      src = prev.fetchFromGitHub {
-        owner = "siderolabs";
-        repo = "talos";
-        rev = "v1.5.1";
-        hash = "sha256-HYIk1oZbtcnHLap+4AMwoQN0k44zjiiwDzGcNW+9qqM=";
-      };
-      vendorHash = "sha256-Aefwa8zdKWV9TE9rwNA4pzKZekTurkD0pTDm3QfKdUQ=";
-    });
-  };
+  # talosctl = prev.talosctl.override {
+  #   buildGoModule = args: prev.buildGoModule (args // {
+  #     version = "1.5.1";
+  #     src = prev.fetchFromGitHub {
+  #       owner = "siderolabs";
+  #       repo = "talos";
+  #       rev = "v1.5.1";
+  #       hash = "sha256-HYIk1oZbtcnHLap+4AMwoQN0k44zjiiwDzGcNW+9qqM=";
+  #     };
+  #     vendorHash = "sha256-Aefwa8zdKWV9TE9rwNA4pzKZekTurkD0pTDm3QfKdUQ=";
+  #   });
+  # };
   # nil = prev.nil.overrideAttrs (old: rec {
   #   pname = "nil";
   #   version = "2023-03-01";
