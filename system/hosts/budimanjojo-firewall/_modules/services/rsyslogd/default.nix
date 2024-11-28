fb6300dd56c963a0fceca7c4c28b639b9caa7fdd
budimanjojo <budimanjojo@gmail.com>
2024-11-28 16:40:30 +0700

    fix(system): fix rsyslog not working correctly
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-firewall/_modules/services/rsyslogd/default.nix
+++ system/hosts/budimanjojo-firewall/_modules/services/rsyslogd/default.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ config, ... }:
 {
   services = {
     journald.forwardToSyslog = true;
     rsyslogd = {
       enable = true;
       defaultConfig = ''
+        # for some reason rsyslog shows the hostname as "localhost" which is not helpful
+        global(localHostname="${config.networking.hostName}")
+
+        # TOOD: the options are needed because of how `vector` is setup in my kubernetes cluster
+        # I will remove them once I updated the vector part
+        module(load="imklog" ParseKernelTimestamp="on" KeepKernelTimestamp="on")
+
         kern.warning @@(o)192.168.15.5:6000;RSYSLOG_SyslogProtocol23Format
       '';
     };
