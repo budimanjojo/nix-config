1c5506543126d57667345de52c923904fd9494c6
budimanjojo <budimanjojo@gmail.com>
2024-11-28 23:43:46 +0700

    fix(system): fix issue with dns for podman container
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-firewall/_modules/podman.nix
+++ system/hosts/budimanjojo-firewall/_modules/podman.nix
@@ -7,7 +7,9 @@
       enable = true;
       defaultNetwork.settings = {
         network_interface = "ctr0";
-        dns_enabled = true;
+        # setting this to true ironically breaks the dns because dns server is not running in the gateway ip
+        # even setting `dns_name_servers` here doesn't work with this set to true
+        dns_enabled = false;
         subnets = [
           {
             subnet = "10.5.0.0/24";
