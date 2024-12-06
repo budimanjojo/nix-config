0c85e857113a5db1d67273611561c00613963a98
budimanjojo <budimanjojo@gmail.com>
2024-12-06 11:41:45 +0700

    feat(system): add `restic-backup` module
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-firewall/_modules/services/omada-controller/default.nix
+++ system/hosts/budimanjojo-firewall/_modules/services/omada-controller/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, ... }:
+let
+  app = "omada-controller";
+  dataDir = "/var/lib/omada-controller-data";
+  logsDir = "/var/lib/omada-controller-logs";
+in
 {
   systemd.tmpfiles.rules = [
-    "d /var/lib/omada-controller-data 0755 508 508"
-    "d /var/lib/omada-controller-logs 0755 508 508"
+    "d ${dataDir} 0755 508 508"
+    "d ${logsDir} 0755 508 508"
   ];
   virtualisation.oci-containers.containers.omada-controller = {
     image = "docker.io/mbentley/omada-controller:5.14";
@@ -11,11 +16,20 @@
       "--stop-timeout=30"
     ];
     volumes = [
-      "/var/lib/omada-controller-data:/opt/tplink/EAPController/data:rw"
-      "/var/lib/omada-controller-logs:/opt/tplink/EAPController/logs:rw"
+      "${dataDir}:/opt/tplink/EAPController/data:rw"
+      "${logsDir}:/opt/tplink/EAPController/logs:rw"
     ];
     environment = {
       TZ = "Asia/Jakarta";
     };
+  };
+
+  services.restic.backups = config.lib.mySystem.mkRestic {
+    inherit app;
+    paths = [
+      "${dataDir}"
+      "${logsDir}"
+    ];
+    excludePaths = [ "autobackup" ];
   };
 }
