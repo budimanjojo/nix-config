18397c23621784dafcc366b07c346a48256826f3
budimanjojo <budimanjojo@gmail.com>
2024-12-11 11:14:31 +0700

    feat(code): reduce code nesting
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-nas/disk-config.nix
+++ system/hosts/budimanjojo-nas/disk-config.nix
@@ -2,61 +2,59 @@
 {
   config = {
     # filesystems managed by Disko are defined here
-    disko.devices = {
-      disk = {
-        main = {
-          type = "disk";
-          device = "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026T0910A0397";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                priority = 1;
-                name = "ESP";
-                start = "1M";
-                end = "512M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
+    disko.devices.disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026T0910A0397";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
               };
-              root = {
-                size = "100%";
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ]; # override existing partiion
-                  subvolumes = {
-                    "/rootfs" = {
-                      mountpoint = "/";
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # override existing partiion
+                subvolumes = {
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
                   };
                 };
               };
             };
           };
         };
+      };
 
-        # full btrfs drive for incus
-        vm-data = {
-          type = "disk";
-          device = "/dev/disk/by-id/ata-Kingmax_SSD_240GB_1393070A113C00097082";
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ]; # override existing partition
-            mountpoint = "/vm-data";
-          };
+      # full btrfs drive for incus
+      vm-data = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-Kingmax_SSD_240GB_1393070A113C00097082";
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ]; # override existing partition
+          mountpoint = "/vm-data";
         };
       };
     };
