f4b85d279fc75d9071d969e9e38893f99a912e2c
budimanjojo <budimanjojo@gmail.com>
2024-12-12 13:31:44 +0700

    fix(nixvim): remove warnings from gitsigns.nvim
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- home/budiman/config/neovim/lua/plugin-configs/_gitsigns.lua
+++ home/budiman/config/neovim/lua/plugin-configs/_gitsigns.lua
@@ -1,10 +1,10 @@
 require('gitsigns').setup {
   signs = {
-    add          = { hl = 'GitSignsAdd', text = '󰐕', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change       = { hl = 'GitSignsChange', text = '󰐕', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    add          = { text = '󰐕' },
+    change       = { text = '󰐕' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
   },
   linehl = true,
   current_line_blame = true,
