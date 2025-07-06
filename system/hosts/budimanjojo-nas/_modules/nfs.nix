cb6ec028529460b8e422da09cf81e44715b1d239
budimanjojo <budimanjojo@gmail.com>
2025-07-06 23:00:20 +0700

    feat(system): use `no_subtree_check` instead for nfs server
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-nas/_modules/nfs.nix
+++ system/hosts/budimanjojo-nas/_modules/nfs.nix
@@ -14,7 +14,7 @@
   services.nfs.server = {
     enable = true;
     exports = ''
-      /export/Kubernetes-Volumes (fsid=0241f5d9-633f-457a-9f48-dbbbf9d5adcb,rw,subtree_check,insecure,no_root_squash,crossmnt,async)
+      /export/Kubernetes-Volumes (fsid=0241f5d9-633f-457a-9f48-dbbbf9d5adcb,rw,no_subtree_check,insecure,no_root_squash,crossmnt,async)
       /export (ro,fsid=0,root_squash,no_subtree_check,hide)
     '';
   };
