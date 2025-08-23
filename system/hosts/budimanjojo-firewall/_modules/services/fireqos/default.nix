b5ada2b20c899ea84f26586cd3d7ca009044e2fd
budimanjojo <budimanjojo@gmail.com>
2025-08-23 16:37:45 +0700

    feat(system): upgraded my internet speed
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-firewall/_modules/services/fireqos/default.nix
+++ system/hosts/budimanjojo-firewall/_modules/services/fireqos/default.nix
@@ -3,7 +3,7 @@
   services.fireqos = {
     enable = true;
     config = ''
-      interface wan0 world-in input rate 50mbit
+      interface wan0 world-in input rate 200mbit
         class calls commit 300kbit
           match ports 3478:3497,16384:16387,16393:16402
 
@@ -12,7 +12,7 @@
         class torrents
           match port 50413
 
-      interface wan0 world-out output rate 15mbit
+      interface wan0 world-out output rate 65mbit
         class calls commit 300kbit
           match ports 3478:3497,16384:16387,16393:16402
 
