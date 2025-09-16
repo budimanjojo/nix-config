0b3554d0af541c2c428287e571ff11566033c970
budimanjojo <budimanjojo@gmail.com>
2025-09-16 10:49:46 +0700

    feat(system): increase `max-nar-info-size` for `attic`
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-nas/_modules/attic.nix
+++ system/hosts/budimanjojo-nas/_modules/attic.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   sops.secrets.atticd-env-file = {
     sopsFile = ./secret.sops.yaml;
@@ -6,10 +6,12 @@
 
   services.atticd = {
     enable = true;
+    package = pkgs.unstable.attic-server;
     environmentFile = "${config.sops.secrets.atticd-env-file.path}";
     settings = {
       listen = "[::]:8080";
       garbage-collection.default-retention-period = "3 months";
+      max-nar-info-size = 3145728; # 3 MiB
     };
   };
 
