fcf2cd13c475037d260ea3515d05309ea0328ffd
budimanjojo <budimanjojo@gmail.com>
2025-11-12 16:36:14 +0700

    feat(neovim): add custom `trim_trailing_lastline` editorconfig property
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- home/budiman/config/neovim/plugins/mini/trailspace.nix
+++ home/budiman/config/neovim/plugins/mini/trailspace.nix
@@ -1,3 +1,30 @@
 {
   plugins.mini.modules.trailspace = { };
+
+  # custom `trim_trailing_lastline` editorconfig property
+  # that will remove trailing last lines from file
+  # based on the `trim_trailing_whitespace`
+  editorconfig.properties.trim_trailing_lastline = ''
+    function(bufnr, val)
+      assert(
+        val == 'true' or val == 'false',
+        'trim_trailing_lastline must be either "true" or "false"'
+      )
+
+      if val == 'true' then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = 'nvim.editorconfig',
+          buffer = bufnr,
+          callback = function()
+            MiniTrailspace.trim_last_lines()
+          end,
+        })
+      else
+        vim.api.nvim_clear_autocmds({
+          event = 'BufWritePre',
+          group = 'nvim.editorconfig',
+        })
+      end
+    end
+  '';
 }
