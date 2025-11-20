a93a4f506ded91a3538bd26acc6b732211e35ad2
budimanjojo <budimanjojo@gmail.com>
2025-11-20 16:39:56 +0700

    test
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/_modules/_default/default.nix
+++ system/_modules/_default/default.nix
@@ -1,4 +1,10 @@
-{ config, hostname, ... }:
+{
+  config,
+  hostname,
+  pkgs,
+  lib,
+  ...
+}:
 let
   mySystem = config.mySystem;
 in
@@ -29,5 +35,33 @@ in
     # do not change unless you know what you are doing
     system.stateVersion = "23.11";
     documentation.nixos.enable = false;
+
+    environment.etc."man_db.conf".text =
+      let
+        cfg = config.documentation.man.man-db;
+        buildPackage = cfg.package;
+        manualCache =
+          pkgs.runCommand "man-cache"
+            {
+              nativeBuildInputs = [ buildPackage ];
+              preferLocalBuild = true;
+            }
+            ''
+              echo "MANDB_MAP ${cfg.manualPages}/share/man $out" > man.conf
+              mandb -C man.conf -psc
+            '';
+      in
+      lib.mkOverride 999 ''
+        # Manual pages paths for NixOS
+        MANPATH_MAP /run/current-system/sw/bin /run/current-system/sw/share/man
+        MANPATH_MAP /run/wrappers/bin          /run/current-system/sw/share/man
+
+        ${lib.optionalString config.documentation.man.generateCaches ''
+          # Generated manual pages cache for NixOS (immutable)
+          MANDB_MAP /run/current-system/sw/share/man ${manualCache}
+        ''}
+        # Manual pages caches for NixOS
+        MANDB_MAP /run/current-system/sw/share/man /var/cache/man/nixos
+      '';
   };
 }
