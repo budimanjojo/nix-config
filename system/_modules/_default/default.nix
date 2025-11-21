39d1d1f8972b3dc8294100b5601ddc178943129a
budimanjojo <budimanjojo@gmail.com>
2025-11-21 12:17:48 +0700

    feat(system): remove test codes
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/_modules/_default/default.nix
+++ system/_modules/_default/default.nix
@@ -35,32 +35,5 @@ in
     # do not change unless you know what you are doing
     system.stateVersion = "23.11";
     documentation.nixos.enable = false;
-
-    environment.etc."man_db.conf".text =
-      let
-        cfg = config.documentation.man.man-db;
-        buildPackage = cfg.package;
-        manualCache =
-          pkgs.runCommand "man-cache-test"
-            {
-              nativeBuildInputs = [ buildPackage ];
-            }
-            ''
-              echo "MANDB_MAP ${cfg.manualPages}/share/man $out" > man.conf
-              mandb -C man.conf -psc
-            '';
-      in
-      lib.mkForce ''
-        # Manual pages paths for NixOS
-        MANPATH_MAP /run/current-system/sw/bin /run/current-system/sw/share/man
-        MANPATH_MAP /run/wrappers/bin          /run/current-system/sw/share/man
-
-        ${lib.optionalString config.documentation.man.generateCaches ''
-          # Generated manual pages cache for NixOS (immutable)
-          MANDB_MAP /run/current-system/sw/share/man ${manualCache}
-        ''}
-        # Manual pages caches for NixOS
-        MANDB_MAP /run/current-system/sw/share/man /var/cache/man/nixos
-      '';
   };
 }
