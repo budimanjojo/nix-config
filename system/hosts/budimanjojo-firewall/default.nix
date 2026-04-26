c257985b65acbb9fef620679336de664f78acb52
budimanjojo <budimanjojo@gmail.com>
2026-04-26 13:44:37 +0700

    feat(system): make restic backup to nfs instead
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- system/hosts/budimanjojo-firewall/default.nix
+++ system/hosts/budimanjojo-firewall/default.nix
@@ -20,9 +20,30 @@
           enable = true;
           fileSystems = [ "/" ];
         };
-        restic-backup.enable = true;
+        restic-backup = {
+          enable = true;
+          location = "/mnt/backup-nfs";
+        };
       };
     };
+
+    boot.supportedFilesystems = [ "nfs" ];
+    services.rpcbind.enable = true;
+    systemd.mounts = [
+      {
+        type = "nfs";
+        what = "192.168.200.30:/Backups/restic-budimanjojo-firewall";
+        where = "/mnt/backups-nfs";
+        options = "nfsvers=4.2,nolock,hard,rw,noatime,nconnect=8";
+      }
+    ];
+    systemd.automounts = [
+      {
+        wantedBy = [ "multi-user.target" ];
+        automountConfig.TimeoutIdleSec = "600";
+        where = "/mnt/backups-nfs";
+      }
+    ];
 
     services.openssh.listenAddresses = [
       { addr = "192.168.200.1"; }
