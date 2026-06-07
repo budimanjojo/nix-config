02a514f0ad2eedde010530c169adc727e7f46ee3
budimanjojo <budimanjojo@gmail.com>
2026-06-07 09:34:47 +0700

    fix(nixvim): this should now fix the check error
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- modules/modules/generic/nixvim/plugins/treesitter.nix
+++ modules/modules/generic/nixvim/plugins/treesitter.nix
@@ -3,19 +3,9 @@
     plugins.treesitter = {
       enable = true;
       nixvimInjections = true;
-      settings = {
-        highlight = {
-          enable = true;
-          disable.__raw = ''
-            function(_, bufnr)
-              if string.find(vim.bo.filetype, 'chezmoitmpl') then
-                return true
-              end
-
-              return vim.api.nvim_buf_line_count(bufnr) > 50000
-            end
-          '';
-        };
+      highlight = {
+        enable = true;
+        disable = [ "chezmoitmpl" ];
       };
     };
   };
