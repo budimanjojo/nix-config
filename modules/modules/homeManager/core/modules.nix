f822da0b468d453242038136b7a53a9d21ba3aaa
budimanjojo <budimanjojo@gmail.com>
2026-06-08 12:13:35 +0700

    feat: use whiskers package from upstream nixpkgs so we don't build it
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- modules/modules/homeManager/core/modules.nix
+++ modules/modules/homeManager/core/modules.nix
@@ -1,7 +1,13 @@
 { inputs, ... }:
 {
-  flake.modules.homeManager.core.imports = [
-    inputs.sops-nix.homeManagerModules.sops
-    inputs.catppuccin.homeModules.catppuccin
-  ];
+  flake.modules.homeManager.core = { pkgs, ... }: {
+    imports = [
+      inputs.sops-nix.homeManagerModules.sops
+      inputs.catppuccin.homeModules.catppuccin
+    ];
+    # use whiskers package from nixpkgs so we don't need to build it
+    catppuccin.sources = inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.overrideScope (
+      final: prev: { whiskers = pkgs.catppuccin-whiskers; }
+    );
+  };
 }
--- modules/modules/nixos/core/modules.nix
+++ modules/modules/nixos/core/modules.nix
@@ -1,7 +1,12 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.core.imports = [
-    inputs.sops-nix.nixosModules.sops
-    inputs.catppuccin.nixosModules.catppuccin
-  ];
+  flake.modules.nixos.core = { pkgs, ... }: {
+    imports = [
+      inputs.sops-nix.nixosModules.sops
+      inputs.catppuccin.nixosModules.catppuccin
+    ];
+    catppuccin.sources = inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.overrideScope (
+      final: prev: { whiskers = pkgs.catppuccin-whiskers; }
+    );
+  };
 }
