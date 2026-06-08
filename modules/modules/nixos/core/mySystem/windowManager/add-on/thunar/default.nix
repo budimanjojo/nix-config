83370577a1f8d9c329b193255631354b083e24f8
budimanjojo <budimanjojo@gmail.com>
2026-06-08 12:15:04 +0700

    feat: thunar plugins moved to top level
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- modules/modules/nixos/core/mySystem/windowManager/add-on/thunar/default.nix
+++ modules/modules/nixos/core/mySystem/windowManager/add-on/thunar/default.nix
@@ -17,7 +17,7 @@
       config = lib.mkIf (cfg.enable) {
         programs.thunar = {
           enable = true;
-          plugins = with pkgs.xfce; [
+          plugins = with pkgs; [
             thunar-volman
             thunar-archive-plugin
             thunar-media-tags-plugin
