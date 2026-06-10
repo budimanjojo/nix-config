3c483c94fa505af5c2cdf7019b5b63f770c11cce
budimanjojo <budimanjojo@gmail.com>
2026-06-10 10:23:17 +0700

    feat(system): bad typo
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- modules/nixosHosts/budimanjojo-firewall/services/rsyslogd.nix
+++ modules/nixosHosts/budimanjojo-firewall/services/rsyslogd.nix
@@ -10,11 +10,11 @@
             # for some reason rsyslog shows the hostname as "localhost" which is not helpful
             global(localHostname="${config.networking.hostName}")
 
-            # TODO: the options are needed because of how `vector` is setup in my kubernetes cluster
+            # TOOD: the options are needed because of how `vector` is setup in my kubernetes cluster
             # I will remove them once I updated the vector part
             module(load="imklog" ParseKernelTimestamp="on" KeepKernelTimestamp="on")
 
-            kern.warning @@(o)192.168.15.5:6000;RSYSLOG_SyslogProtocol123Format
+            kern.warning @@(o)192.168.15.5:6000;RSYSLOG_SyslogProtocol23Format
           '';
         };
       };
