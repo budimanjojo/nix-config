26c4609970f4853d3fa146cf9405fad664274c80
budimanjojo <budimanjojo@gmail.com>
2026-06-11 08:06:50 +0700

    feat(home): hardcode catppuccin_mocha starship color palette
    IFD issue is not yet fixed for catppuccin nix
    
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- modules/modules/homeManager/core/myHome/shell/starship/default.nix
+++ modules/modules/homeManager/core/myHome/shell/starship/default.nix
@@ -15,13 +15,41 @@
       };
 
       config = lib.mkIf (cfg.enable) {
-        # TODO: wait for the IFD issue is fixed
-        # ref: https://github.com/catppuccin/nix/issues/392
-        catppuccin.starship.enable = lib.mkIf (pkgs.stdenv.hostPlatform.system == "x86_64-linux") true;
-
         programs.starship = {
           enable = true;
           settings = {
+            # TODO: wait for the IFD issue is fixed
+            # ref: https://github.com/catppuccin/nix/issues/392
+            palette = "catppuccin_mocha";
+            palettes.catppuccin_mocha = {
+              rosewater = "#f5e0dc";
+              flamingo = "#f2cdcd";
+              pink = "#f5c2e7";
+              mauve = "#cba6f7";
+              red = "#f38ba8";
+              maroon = "#eba0ac";
+              peach = "#fab387";
+              yellow = "#f9e2af";
+              green = "#a6e3a1";
+              teal = "#94e2d5";
+              sky = "#89dceb";
+              sapphire = "#74c7ec";
+              blue = "#89b4fa";
+              lavender = "#b4befe";
+              text = "#cdd6f4";
+              subtext1 = "#bac2de";
+              subtext0 = "#a6adc8";
+              overlay2 = "#9399b2";
+              overlay1 = "#7f849c";
+              overlay0 = "#6c7086";
+              surface2 = "#585b70";
+              surface1 = "#45475a";
+              surface0 = "#313244";
+              base = "#1e1e2e";
+              mantle = "#181825";
+              crust = "#11111b";
+            };
+
             add_newline = false;
             command_timeout = 1000;
             format = lib.concatStrings [
